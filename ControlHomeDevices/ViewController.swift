88197c7ef0458095e36c6bc9cad45a679e63dfb3
elhoussaine elgamouz <elhoussaineelgamouz@gmail.com>
2024-12-26 11:14:52 +0100

    Initial Commit

---
+++ ControlHomeDevices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ControlHomeDevices
+//
+//  Created by El houssaine El GAMOUZ on 26/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
